0ad6900c90cd377e2ef9220cad8683677ec201f9
Andrew Montalenti <andrew@parsely.com>
2015-01-18 14:27:43 -0500

    sync up

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request
-from rapid import (top_articles, search_articles, insert_article)
+from flask import Flask, render_template, request, redirect, abort
+from flask.ext.script import Manager
+from rapid import (top_articles, search_articles, insert_article, validate_submission, track_click)
+from filters import human_date
 
 app = Flask(__name__, static_folder="../static", static_url_path="/static")
+app.debug = True
+app.add_template_filter(human_date)
 
 @app.route('/')
 def index():
     articles = top_articles()
     return render_template('index.jinja2.html', 
-                           rows=articles)
+                           rows=articles,
+                           page_links="active")
 
 @app.route('/search/<query>')
 def search(query):
@@ -21,14 +26,32 @@ def submit():
     if request.method == "POST":
         return do_submit()
     else:
-        return render_template('submit.jinja2.html')
+        return render_template('submit.jinja2.html',
+                               page_submit="active")
 
 def do_submit():
-    article = insert_article()
-    return render_template("inserted.jinja2.html", article=article)
+    form = request.form
+    submission = dict(
+        title=form["title"],
+        link=form["link"]
+    )
+    valid, errors = validate_submission(submission)
+    if valid:
+        article = insert_article(submission)
+        return render_template("success.jinja2.html",
+                               page_submit="active")
+    else:
+        return render_template('submit.jinja2.html',
+                               page_submit="active",
+                               errors=errors)
 
-def run_devserver():
-    app.run(debug=True)
+@app.route('/click/')
+def click():
+    url = request.args["url"]
+    track_click(url)
+    return redirect(url)
+
+manager = Manager(app)
 
 if __name__ == "__main__":
-    run_devserver()
+    manager.run()
